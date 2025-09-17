abc171a3fde77ec424d6234e32fb10227cdd1fba
David Mwangi <davidmwangi@microsoft.com>
2025-09-17 10:37:40 +0300

    Add SDK Project files

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from modules.redactor import redactor
 
 
-def main(prompt: str, opted_out: bool) -> str:
-    return redactor(prompt, opted_out)
+def main(prompt: str, opted_in: bool = True) -> str:
+    return redactor(prompt, opted_in)
 
 
 if __name__ == "__main__":
     response = main(
-        "My name is Brian, I am blind. I would like to get directions to the nearest grocery store.",
-        True,
+        "My name is Jiminy Cricket with email <Jiminy.Cricket@example.com>, I am blind and deaf and mute. I am also quadriplegia and I would like to get directions to the nearest shopping centre.",
     )
 
     print(response)
