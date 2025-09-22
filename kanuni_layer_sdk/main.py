3b0842aca37da6d902d3bbdea2c4c50b282d100c
davis-mwangi <daudimwash@gmail.com>
2025-09-22 16:06:29 +0300

    Final Changes

--- kanuni_layer_sdk/main.py
+++ kanuni_layer_sdk/main.py
@@ -4,6 +4,8 @@ from .modules import redactor, validate_prompt
 from prompt_library import transform_multiple_keys as transform_prompt
 
 async def reduct(prompt: str, opted_in: bool = True) -> str:
+    print("Raw Prompt:")
+    print(prompt)
     #Validate the prompt input
     print("")
     validated_prompt = validate_prompt(prompt)
@@ -37,5 +39,5 @@ if __name__ == "__main__":
         "My name is Jimmy Cricket with email <Jiminy.Cricket@example.com>, I am blind and deaf and mute. I am also quadriplegia and I would like to get directions to the nearest shopping centre. My credit card number is 4111 1111 1111 1111 and my phone number is +1 (555) 123-4567. Also, my SSN is 123-45-6789.",
        )
     )
-
+    print("Ënhanced Reducted Prompt:")
     print(response)
